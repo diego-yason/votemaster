0810f6831ed20ade3b634da98b0a83b464dfd44f
KingMarine-GH <kingmarineofov@gmail.com>
2021-05-31 23:14:19 +0800

    created start script - probably to be replaced with a better system soon

--- res/clearBuild.js
+++
@@ -1,2 +0,0 @@
-import fs from "fs";
-fs.rmdirSync("../build", { recursive: true });
---
+++ res/runServer.sh
@@ -0,0 +1,11 @@
+cd ..
+
+mv tsconfig.json res
+mv res/tsconfig-start.json tsconfig.json
+
+tsc
+
+mv tsconfig.json res/tsconfig-start.json
+mv res/tsconfig.json . 
+
+node .
